774fa249f9c0ac301c8d2e31961a7410262883e7
dsmith <derek@simplegeo.com>
2011-02-24 15:06:55 -0800

    Allow the key value to be a query string that will be able to filter result based on a key=value format.

--- ext/jsonlookup.rb
+++ ext/jsonlookup.rb
@@ -16,6 +16,18 @@ module Puppet::Parser::Functions
 
   raise Puppet::ParseError, ("jsonlookup(): wrong number of arguments (#{args.length}; must be <= 3)") if args.length > 3
 
+
+  # Parse out the query string, if there is one
+  query_key = nil
+  query_value = nil
+  if query_string
+    components = key.split('=')
+    if components.length == 2
+      query_key = components[0]
+      query_value = components[1]
+    end
+  end
+
   jsonlookup_datadir = lookupvar('jsonlookup_datadir')
   jsonlookup_precedence = Array.new
 
@@ -38,12 +50,24 @@ module Puppet::Parser::Functions
 
   desired = nil
 
+  # The desired effect here:
+  # - If the json file is a list, we return all elements in the list
+  #   that contain the key
+  # - If the json file is a dict, we return the element at the key
+  # - If the json file is a list and the key is a query string, we return
+  #   all the elements that match the key,value pair
   datafiles.each do |file|
     if desired.nil?
       begin
         result = JSON.parse(File.open(file, "r").read)
-        if result.include?(key):
+        if result.kind_of? Hash
           desired = result[key]
+        elsif result.kind_of? Array
+          if query_key and query_value
+            desired = result.select{ |e| e[key] and e[key] == value}
+          elsif result.has_key?(key)
+            desired = result[key]
+          end
         end
       rescue
 	# If we can't open the file we will fail here.
